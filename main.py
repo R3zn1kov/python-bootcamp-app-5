d8498a07050d9ebc1363249b405ac7f59e83723a
R3zn1kov <ldlreznikov@gmail.com>
2024-02-13 16:16:41 +0100

    Send email once the API request is committed

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import requests
+from send_email import send_email
 
-api_hey = "a265d06209854926a52e9e0d6b68450f"
-url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=a265d06209854926a52e9e0d6b68450f"
+topic = "tesla"
+api_key = "890603a55bfa47048e4490069ebee18c"
+url = "https://newsapi.org/v2/everything?"\
+       f"q={topic}&"\
+      "sortBy=publishedAt&"\
+      "apiKey= 890603a55bfa47048e4490069ebee18c&"\
+      "language=en"
 
+# Make request
 request = requests.get(url)
+
+# Get a dictionary with data
 content = request.json()
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+
+# Access the article titles and description
+body = ""
+for article in content["articles"] [0:20]:
+    if article["title"] is not None:
+        body = "Subject: Today's news" body + article["title"] + \
+             "\n" + str(article["description"]) +  \
+             "\n" + article["url"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
